d41b51fc6cdb82c1434831133e8617da3ca09845
Samuel Hunter <samuelhunter1024@gmail.com>
2020-11-30 21:10:11 -0800

    Solve day 1

--- day01.lisp
+++ day01.lisp
@@ -7,8 +7,22 @@
 
 
 
-(defparameter +input+ (read-puzzle-text))
+(defparameter +input+ (read-puzzle-sexp))
 
-(defun solve-part-1 ())
+(defun solve-part-1 ()
+  (loop :for nums :on +input+
+        :for x := (first nums)
+        :do (loop :for y :in (rest nums)
+                  :when (= 2020 (+ x y))
+                    :do (return-from solve-part-1
+                          (values (* x y) x y)))))
 
-(defun solve-part-2 ())
+(defun solve-part-2 ()
+  (loop :for nums :on +input+
+        :for x := (first nums)
+        :do (loop :for nums2 :on (rest nums)
+                  :for y := (first nums2)
+                  :do (loop :for z :in (rest nums2)
+                            :when (= 2020 (+ x y z))
+                              :do (return-from solve-part-2
+                                    (* x y z))))))
