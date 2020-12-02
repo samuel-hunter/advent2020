160050c496bc9e677d0da233eaa01fed42410187
Samuel Hunter <samuelhunter1024@gmail.com>
2020-12-01 21:41:40 -0800

    Refactor for Day 2

--- day02.lisp
+++ day02.lisp
@@ -1,6 +1,6 @@
 
 (defpackage #:advent2020.day02
-  (:use #:cl #:alexandria #:advent2020.util #:split-sequence)
+  (:use #:cl #:alexandria #:advent2020.util #:arrows)
   (:export #:solve-part-1 #:solve-part-2))
 
 (in-package #:advent2020.day02)
@@ -8,36 +8,43 @@
 
 
 (defun parse-input (input)
-  (loop :for line :in (split-sequence #\Newline input)
-        :until (string= line "")
-        :for (min max letter password)
-          := (coerce (second (multiple-value-list (ppcre:scan-to-strings "(\\d+)-(\\d+) (\\w): (\\w+)"
-                                                                                                      line)))
-                                                  'list)
-        :collect (list :min (parse-integer min)
-                       :max (parse-integer max)
-                       :letter (char letter 0)
-                       :password password)))
+  (with-input-from-string (stream input)
+    (loop :with scanner := (ppcre:create-scanner "(\\d+)-(\\d+) (\\w): (\\w+)")
+          :for line := (read-line stream nil)
+          :while line
+
+          ;; Scan the substrings of the line.
+          :for (num1 num2 letter password)
+            := (-<>
+                ;; Scan each line -- scan-to-strings returns multiple
+                ;; values
+                (multiple-value-list
+                 (ppcre:scan-to-strings scanner line))
+                ;; ...and we want the second one.
+                (second <>)
+                ;; Convert to list.
+                (coerce <> 'list))
+          ;; Pack it back into a list.
+          :collect (list (parse-integer num1)
+                         (parse-integer num2)
+                         (char letter 0) ;; Convert letter "string" to a char.
+                         password))))
 
 (defparameter +input+ (parse-input (read-puzzle-text)))
 
 (defun count-occurrences (char string)
-  (loop :for i :below (length string)
-        :for c := (char string i)
-        :counting (char= c char)))
+  "Count the occurences of CHAR in STRING."
+  (loop :for c :across string
+        :count (char= c char)))
 
 (defun solve-part-1 ()
-  (loop :for pass :in +input+
-        :do (print pass)
-        :counting (<= (getf pass :min)
-                      (count-occurrences (getf pass :letter)
-                                         (getf pass :password))
-                      (getf pass :max))))
+  (loop :for (min max letter password) :in +input+
+        :for occurrences := (count-occurrences letter password)
+        :count (<= min occurrences max)))
 
 (defun solve-part-2 ()
-  (loop :for pass :in +input+
-        :do (print pass)
-        :counting (xor (char= (getf pass :letter)
-                              (char (getf pass :password) (1- (getf pass :min))))
-                       (char= (getf pass :letter)
-                              (char (getf pass :password) (1- (getf pass :max)))))))
+  (loop :for (pos1 pos2 letter password) :in +input+
+        :count (xor (char= letter
+                           (char password (1- pos1)))
+                    (char= letter
+                           (char password (1- pos2))))))
