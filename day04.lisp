6fe388b5654049912980a46ac1ecc65680aa6eca
Samuel Hunter <samuelhunter1024@gmail.com>
2020-12-03 18:55:35 -0800

    Set up Day 4

---
+++ day04.lisp
@@ -0,0 +1,14 @@
+
+(defpackage #:advent2020.day04
+  (:use #:cl #:alexandria #:advent2020.util #:arrows)
+  (:export #:solve-part-1 #:solve-part-2))
+
+(in-package #:advent2020.day04)
+
+
+
+(defparameter +input+ (read-puzzle-text))
+
+(defun solve-part-1 ())
+
+(defun solve-part-2 ())
