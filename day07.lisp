bb7eaa8f984a1a5d66b790525d78c32791be4d3b
Samuel Hunter <samuelhunter1024@gmail.com>
2020-12-06 21:50:32 -0800

    Refactor and document Day 7

--- day07.lisp
+++ day07.lisp
@@ -7,46 +7,60 @@
 
 
 
-(defun kw (str)
-  (make-keyword (string-upcase str)))
+(defun make-color (str1 str2)
+  "Format and intern a color combination into a keyword."
+  ;; Technically bad-practice to dynamically intern symbols. from user
+  ;; input, buuuut this is Advent of Code, and so I'll deliberately
+  ;; violate this security concern to intern 595-ish symbols.
+  (make-keyword (format nil "~A-~A"
+                        (string-upcase str1)
+                        (string-upcase str2))))
 
-(defun parse-luggage (line)
+(defun parse-bag (line)
+  "Convert a text line of a bag into a list of its color, followed by
+an associative list of child bag colors and their quantities."
   (let* ((sentence (split-sequence #\Space line))
-         (color (cons (kw (first sentence))
-                      (kw (second sentence)))))
+         (color (make-color (first sentence)
+                            (second sentence))))
     (cons color
+          ;; Iterate through every four words (with the first four
+          ;; skipped), and bind the first three to (num color1 color2)
           (loop :for (num color1 color2) :on (cddddr sentence) :by #'cddddr
+                ;; Some bags sometimes start with "X Y bags contain no
+                ;; other bags." Return an empty list instead.
                 :if (string= num "no")
-                  :do (return nil)
+                  :do (return ())
                 :else
-                  :collect (cons (cons (kw color1) (kw color2))
+                  ;; Return a pair of the bag child's color and the
+                  ;; quantity the parent holds.
+                  :collect (cons (make-color color1 color2)
                                  (parse-integer num))))))
 
-(defun make-luggage-table (luggages)
-  (loop :with table := (make-hash-table :test 'equal)
-        :for (color . items) :in luggages
-        :do (setf (gethash color table) items)
-        :finally (return table)))
-
 (defparameter +input+
-  (make-luggage-table (parse-lines 'parse-luggage)))
+  ;; Because each parsed line has a color as the head of the list, a
+  ;; list of these lines turn into an associative list. Convert this
+  ;; into a hash table.
+  (alist-hash-table (parse-lines 'parse-luggage)))
 
 (defun bag-items (color)
   (gethash color +input+))
 
-(defun has-shiny (bag-color target)
-  (loop :for (color . quantity) :in (bag-items bag-color)
-          :thereis (or (equal target color)
-                       (has-shiny color target))))
+(defun has-color (bag target-color)
+  "Return whether BAG indirectly contains TARGET-COLOR."
+  (loop :for (color . quantity) :in bag
+          :thereis (or (equal target-color color)
+                       (has-color (bag-items color) target-color))))
 
 (defun solve-part-1 ()
-  (loop :for color :being :the :hash-key :of +input+
-        :count (has-shiny color '(:shiny . :gold))))
+  ;; Count all bags that has the shiny gold bag.
+  (loop :for bag :being :the :hash-value :of +input+
+        :count (has-color bag :shiny-gold)))
 
 (defun bag-capacity (color)
+  "Return the number of bags the bag colored COLOR contains."
   (loop :for (color . quantity) :in (bag-items color)
         :sum (* (1+ (bag-capacity color))
                 quantity)))
 
 (defun solve-part-2 ()
-  (bag-capacity '(:shiny . :gold)))
+  (bag-capacity :shiny-gold))
