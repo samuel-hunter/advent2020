004767cd94eba1129dd924e768f2621e1e6d69f6
Samuel Hunter <samuelhunter1024@gmail.com>
2020-12-07 21:44:44 -0800

    Refactor day 8

--- day08.lisp
+++ day08.lisp
@@ -1,65 +1,72 @@
 
 (defpackage #:advent2020.day08
-  (:use #:cl #:alexandria #:advent2020.util #:arrows #:split-sequence)
+  (:use #:cl #:alexandria #:advent2020.util)
   (:export #:solve-part-1 #:solve-part-2))
 
 (in-package #:advent2020.day08)
 
 
 
-(defparameter +input+ (coerce (read-puzzle-sexp "day08.lisp")
-                              'vector))
+(defparameter +input+
+  (coerce (read-puzzle-sexp) 'vector))
 
-(defun solve-part-1 (&optional (code +input+))
-  (loop :with accumulator := 0
-        :with instrs := (make-hash-table :test 'equal)
-        :for i := 0 :then (1+ i)
-        :until (or (gethash i instrs)
-                   (= i (array-dimension code 0)))
-        :for (op num) := (aref code i)
-
-        :do (print (list i op num))
-        :do (setf (gethash i instrs) t)
-            (ecase op
-                 (:acc (incf accumulator num))
-                 (:jmp (incf i (1- num)))
-                 (:nop))
-        :finally (return accumulator)))
+(defun instr-op (instr)
+  (car instr))
+(defun (setf instr-op) (new-value instr)
+  (setf (car instr) new-value))
 
 (defun evaluate (code)
-  (loop :with accumulator := 0
+  "Evaluate CODE and return a list of two values: the value of the
+accumulator, and whether the program terminated normally."
+  (loop ;; Keep track of all instructions that the evaluator has seen before.
         :with instrs := (make-hash-table :test 'equal)
-        :for i := 0 :then (1+ i)
-        :for (op num) := (when (< i (array-dimension code 0))
-                           (aref code i))
-        :until (gethash i instrs)
 
-        :when (= i (array-dimension code 0))
-          :do (return t)
+        ;; op-code registers
+        :with acc := 0
+        :for isp := 0 :then (1+ isp)
 
-        :do (list i op num)
-        :do (setf (gethash i instrs) t)
+        ;; Stop when the instruction was seen before, or if isp
+        ;; reached the end of the code.
+        :until (or (gethash isp instrs)
+                   (= isp (length code)))
+        :for (op num) := (aref code isp)
+
+        :do (setf (gethash isp instrs) t) ;; remember this instruction for later.
+            ;; interpret the op-code.
             (ecase op
-                 (:acc (incf accumulator num))
-                 (:jmp (incf i (1- num)))
-                 (:nop))))
+              (:acc (incf acc num))
+              (:jmp (incf isp (1- num)))
+              (:nop))
+        :finally (return (list acc
+                               (= isp (length code))))))
+
+(defun solve-part-1 ()
+  (first (evaluate +input+)))
+
+(defun find-swappable (code &optional (start 0))
+  "Return the index of the next swappable instruction."
+  (loop :for i :from start :to (length code)
+        :until (member (instr-op (aref code i)) '(:jmp :nop))
+        :finally (return i)))
 
 (defun swap-instr (code indx)
   (let ((instr (aref code indx)))
-    (print (list :swap indx instr))
-    (setf (car instr)
-          (ecase (car instr)
+    (setf (instr-op instr)
+          (ecase (instr-op instr)
             (:nop :jmp)
             (:jmp :nop)))))
 
 (defun solve-part-2 ()
-  (let ((code (copy-seq +input+)))
-    (loop :for i :below (array-dimension code 0)
-          :for instr := (aref code i)
-          :when (member (car instr) '(:jmp :nop))
-            :do (progn
-                  (print (list :loop i instr)))
-                  (swap-instr code i)
-                  (when (evaluate code)
-                    (return (solve-part-1 code)))
-                  (swap-instr code i))))
+  ;; Keep swapping jmp/nop instructions until the code terminated
+  ;; normally.
+  (loop :with code := (copy-seq +input+)
+        :for i := (find-swappable code)
+          :then (find-swappable code (1+ i))
+        :for (acc terminated-normally)
+          := (prog2
+                 (swap-instr code i)
+                 (evaluate code)
+               (swap-instr code i))
+
+        :until terminated-normally
+        :finally (return acc)))
