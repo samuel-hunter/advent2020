166291902c22fb7ffbed52df1d6275b959803908
Samuel Hunter <samuelhunter1024@gmail.com>
2020-12-08 21:31:54 -0800

    Solve day 9

---
+++ day09.lisp
@@ -0,0 +1,45 @@
+
+(defpackage #:advent2020.day09
+  (:use #:cl #:alexandria #:advent2020.util)
+  (:export #:solve-part-1 #:solve-part-2))
+
+(in-package #:advent2020.day09)
+
+
+
+(declaim (optimize (debug 3)))
+
+(defparameter +preamble+ 25)
+(defparameter +input+ (coerce (parse-lines #'parse-integer) 'vector))
+
+
+(defun valid-p (index)
+  (loop :with num := (aref +input+ index)
+        :for i1 :from (- index +preamble+) :below (1- index)
+        :for v1 := (aref +input+ i1)
+          :thereis (loop :for i2 :from (1+ i1) :below index
+                         :for v2 := (aref +input+ i2)
+                           :thereis (= num (+ v1 v2)))))
+
+(defun invalid-p (index)
+  (not (valid-p index)))
+
+(defun solve-part-1 ()
+  (loop :for i :from +preamble+ :below (length +input+)
+        :while (valid-p i)
+        :finally (return (aref +input+ i))))
+
+(defun sums-to (num start)
+  (loop :for i :upfrom start
+        :sum (aref +input+ i) :into acc
+        :until (>= acc num)
+        :finally (return (list (= acc num) (1+ i)))))
+
+(defun solve-part-2 ()
+  (loop :with num := (solve-part-1)
+        :for start :below (length +input+)
+        :for (sums-p end) := (sums-to num start)
+        :until sums-p
+        :finally (let ((seq (subseq +input+ start end)))
+                   (return (+ (reduce #'max seq)
+                       (reduce #'min seq))))))
